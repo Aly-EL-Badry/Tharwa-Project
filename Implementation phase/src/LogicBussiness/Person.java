620d9a63772363b63c6afc9647683cf2d8dda3a2
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2025-05-09 18:26:14 +0300

    changed variable names for readability

--- Implementation phase/src/LogicBussiness/Person.java
+++ Implementation phase/src/LogicBussiness/Person.java
@@ -2,7 +2,7 @@ package LogicBussiness;
 
 import java.util.Vector;
 public class Person {
-   private String username;
+    private String username;
     private String name;
     private String password;
     private String email;
@@ -18,31 +18,31 @@ public class Person {
         return username;
     }
 
-    public void setUsername(String val) {
-        this.username = val;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String val) {
-        this.name = val;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String val) {
-        this.password = val;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String val) {
-        this.email = val;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
