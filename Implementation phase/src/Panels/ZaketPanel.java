7f760d22ed540dec12fc0fa68af92ad14ec3c239
Aly-El-Badry <ali.el.badry.747@gmail.com>
2025-05-12 19:46:24 +0300

    Finishing ZAKAHPANEL

--- Implementation phase/src/Panels/ZaketPanel.java
+++ Implementation phase/src/Panels/ZaketPanel.java
@@ -1,10 +1,12 @@
 package Panels;
 
 import Funcs.HelperFunc;
+import LogicBusiness.Asset;
 import LogicBusiness.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
 
 public class ZaketPanel implements Panel{
 
@@ -14,27 +16,49 @@ public class ZaketPanel implements Panel{
 
     @Override
     public void ViewMenu() {
-        System.out.println("# === Zaket Panel === #");
-
-        String Menu = "1.Zakat Calculation\n2.Halal investment Screen\n3.Tax Complaint\n4.Go back\nEnter your choice :";
-        ArrayList<String> choices = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
-        String choice = HelperFunc.check_menu(Menu, choices);
-
-        if(choice.equals("1"))
-            zakatCalculation();
-        else if (choice.equals("2"))
-            HalalInvestment();
-        else
-            TaxComplaintsRepo();
+        while(true){
+            System.out.println("# === Zaket Panel === #");
+    
+            String Menu = "1.Zakat Calculation\n2.Halal investment Screen\n3.Tax Complaint\n4.Go back\nEnter your choice :";
+            ArrayList<String> choices = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
+            String choice = HelperFunc.check_menu(Menu, choices);
+    
+            if(choice.equals("1"))
+                zakatCalculation();
+            else if (choice.equals("2"))
+                HalalInvestment();
+            else if (choice.equals("3"))
+                TaxComplaintsRepo();
+            else 
+                break;
+        }
     }
 
 
     public void zakatCalculation(){
-        // Calculate Zakah of Bank account
-        double zakatableAmount = 0.0;
+        final double MINIMUM_VALUE_FOR_ZAKET = 7246;
+        final double ZAKAH_PRECENTAGE = 2.5/100;
+        double TotalWealth = 0, Zakah = 0;
 
+        Vector<Asset> userAssets = user.getAssets();
 
+        for(Asset asset : userAssets){
+            TotalWealth += asset.getQuantity() * asset.getPurchasePrice();
+        }
+
+        Zakah = TotalWealth*ZAKAH_PRECENTAGE;
+
+        if(TotalWealth < MINIMUM_VALUE_FOR_ZAKET){
+            System.out.println("You Dont Have to Pay Zakat");
+        } else{
+            System.out.println("Your total wealth is " + TotalWealth);
+            System.out.println("So your Zakah is " + Zakah);
+        }
     }
-    public void HalalInvestment(){}
-    public void TaxComplaintsRepo(){}
+    public void HalalInvestment(){
+        System.out.println("Halal Investment");
+    }
+    public void TaxComplaintsRepo(){
+        System.out.println("Tax Complaints");
+    }
 }
